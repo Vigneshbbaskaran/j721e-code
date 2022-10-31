78e1582a9d180eac047f2415e21bc265d9cb21e8
Vigneshbbaskaran <89230718+Vigneshbbaskaran@users.noreply.github.com>
2022-10-31 10:20:23 +0530

    Add files via upload

--- eMMC/Registers/csd.c
+++ eMMC/Registers/csd.c
@@ -1,1 +1,35 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[])
+{	
+	char buff;
+	int fd,i,j;
+	for(i=1;i<argc;i++)
+	{
+		fd=open(argv[i],O_RDONLY);
+		if(fd<0)
+		{
+			perror("Open fail");
+			_exit(1);
+		}
+		while(1)
+		{
+			j=read(fd,&buff,1);
+			if(j<0)
+			{
+				perror("Read fails..!!");
+				_exit(1);
+			}
+			else if(j==0)
+				break;
+			write(1,&buff,1);
+		}
+		close(fd);
+	}
+	return 0;
+}
